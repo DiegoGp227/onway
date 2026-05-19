9e308dc11ac80cfbf7f8b14c8f9914a285df9625
Diego GP <diego.gp227@gmail.com>
2026-05-18 21:14:42 -0500

    feat: auth routes integrated

--- back/src/routes/index.routes.ts
+++ back/src/routes/index.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import dbCheck from "../modules/test/test.js";
+import { login, signup } from "../modules/auth/auth.controllers.js";
 
 export const router: Router = Router();
 
 // Test Routes
 router.get("/db", dbCheck);
+
+// Auth Routes
+router.post("/signup", signup);
+router.post("/login", login);
