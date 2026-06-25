e61df8f8ce9c73fa12a43871faf617d3632bf879
Diego GP <diego.gp227@gmail.com>
2026-06-24 20:08:48 -0500

    fix(ui): pass workspaceId from TasksSitem to TasksList

--- front/app/components/organism/TasksSitem.tsx
+++ front/app/components/organism/TasksSitem.tsx
@@ -1,11 +1,15 @@
+import { useWorkspaceStore } from "@/store/workspaceStore";
 import TaskHeader from "./TaskHeader";
 import TasksList from "./TasksList";
+import useGetTask from "@/src/home/hooks/useGetTask";
 
 export default function TasksSitem() {
+    const { workspaceId } = useWorkspaceStore();
+
     return (
         <div className="flex-1 min-w-0 flex flex-col overflow-hidden">
-            <TaskHeader />
-            <TasksList />
+            <TaskHeader workspaceId={workspaceId} />
+            <TasksList workspaceId={workspaceId}/>
         </div>
     );
 }
